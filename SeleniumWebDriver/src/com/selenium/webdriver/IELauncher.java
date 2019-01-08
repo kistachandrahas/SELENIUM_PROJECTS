82136add3452fcb67e7b69df37cac6287f99549d
ChandrahasReddyMCA <45177988+ChandrahasReddyMCA@users.noreply.github.com>
2019-01-09 00:42:03 +0530

    line added to IElauncher

--- SeleniumWebDriver/src/com/selenium/webdriver/IELauncher.java
+++ SeleniumWebDriver/src/com/selenium/webdriver/IELauncher.java
@@ -4,6 +4,7 @@ public class IELauncher {
 
 	public static void main(String[] args) {
 		System.out.println("Internet Explorer Launcher");
+		System.setProperty("webdriver.gecko.driver", "E:\\selenium-jars\\geckodriver.exe");
 
 	}
 
